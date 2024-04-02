2f27b59e07d33de440e526df20db03a4b1759788
Rudrax Prajapati <96529581+Rudrax2004@users.noreply.github.com>
2024-04-02 10:07:14 +0530

    final 5

--- MERN_backend/MERN/server/Router/lab-router.js
+++ MERN_backend/MERN/server/Router/lab-router.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const labController = require('../controllers/lab-controller');
 const fyqController = require('../controllers/fyq-Controller');
 
-router.post('https://defendanddetect-production.up.railway.app/api/labs/fyqs', fyqController.createFYQ);
-router.post('https://defendanddetect-production.up.railway.app/api/labs', labController.createLab);
-router.get('https://defendanddetect-production.up.railway.app/api/labs', labController.getAllLabs);
+router.post('/labs/fyqs', fyqController.createFYQ);
+router.post('/labs', labController.createLab);
+router.get('/labs', labController.getAllLabs);
 
 module.exports = router;
