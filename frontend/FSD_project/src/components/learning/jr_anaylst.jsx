95e4268d7075565d470b1c9d70f3ba32a7fbe891
Rudrax Prajapati <96529581+Rudrax2004@users.noreply.github.com>
2024-04-11 11:11:30 +0530

    final changes

--- frontend/FSD_project/src/components/learning/jr_anaylst.jsx
+++ frontend/FSD_project/src/components/learning/jr_anaylst.jsx
@@ -15,7 +15,7 @@ export const JuniorDataSecurityAnalyst = () => {
       <div className="relative mt-8 container bg-black mx-auto group rounded-2xl overflow-hidden border-4 border-gray-500">
         {/* <img className='-z-40 h-full w-full object-cover absolute top-0 left-0 right-0' src="photos/intro_bg.jpg" alt="Introduction Background" /> */}
         <div className="bg-opacity-70 bg-gray-800 p-8">
-          <h1 className="text-3xl font-bold text-yellow-600 mb-8">Introduction to Cybersecurity</h1>
+          <h1 className="text-3xl font-bold text-yellow-600 mb-8">Junior Cybersecurity Analyst</h1>
 
             <AccordionBasicExample index={4}/>
         </div>
