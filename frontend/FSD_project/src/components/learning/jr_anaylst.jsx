9b844ff9a4f68da5a592bb9a1ba23748389a2dd2
Rudrax Prajapati <96529581+Rudrax2004@users.noreply.github.com>
2024-04-11 10:38:06 +0530

    changes in web fundamental lab

--- frontend/FSD_project/src/components/learning/jr_anaylst.jsx
+++ frontend/FSD_project/src/components/learning/jr_anaylst.jsx
@@ -9,17 +9,41 @@ import ParticlesContainer from '../practice/lab1/particle';
 const JuniorDataSecurityAnalyst = () => {
   return (
     <>
-    <Header />
+    <div className='container mt-20 bg-gray-900'>
+    <Header/>
+    <div className="text-gray-300 ">
+      <h2 className='text-3xl font-bold py-5 text-yellow-500'>Junior Data Security Analyst</h2>
+      <p style={{textAlign:'left'}}>
+        A Junior Data Security Analyst plays a critical role in protecting an organization's data assets and ensuring compliance with security standards and regulations. They work closely with senior analysts and security teams to monitor, analyze, and respond to security threats and incidents.
+      </p>
+      <h3 className='text-2xl font-bold py-5 text-green-500'>Responsibilities:</h3>
+      <ul style={{textAlign:'left'}}>
+        <li>Assist in monitoring network and system activities using security tools such as SIEM (Security Information and Event Management) systems.</li>
+        <li>Participate in vulnerability assessments and penetration testing to identify and address security vulnerabilities in systems and applications.</li>
+        <li>Support incident response efforts by analyzing security incidents, containing breaches, and implementing corrective measures.</li>
+        <li>Ensure compliance with data protection regulations (such as GDPR, HIPAA) and industry standards (such as ISO 27001) through regular audits and assessments.</li>
+        <li>Manage and maintain security technologies including firewalls, intrusion detection/prevention systems (IDS/IPS), and endpoint security solutions.</li>
+        <li>Contribute to the development and delivery of security awareness training programs for employees to promote best practices and mitigate security risks.</li>
+        <li>Document security policies, procedures, and incident reports for internal reference and regulatory compliance.</li>
+        <li>Collaborate with cross-functional teams including IT, compliance, and legal departments to address security concerns and implement security controls.</li>
+      </ul>
+      
+      <h3 className='text-2xl font-bold py-5 text-green-500'>Skills and Qualifications:</h3>
+      <ul className='py-3' style={{textAlign:'left'}} >
+        <li className='py-1'>Strong understanding of information security principles, concepts, and best practices.</li>
+        <li className='py-1'>Familiarity with operating systems (Windows, Linux), networking protocols, and security technologies.</li>
+        <li className='py-1'>Proficiency in security tools such as vulnerability scanners (Nessus, Qualys), intrusion detection systems (Snort), and SIEM platforms (Splunk, ELK).</li>
+        <li  className='py-1'>Analytical mindset with the ability to analyze security incidents, identify trends, and recommend improvements.</li>
+        <li className='py-1'>Excellent communication skills, both written and verbal, to effectively communicate security risks and recommendations to stakeholders.</li>
+        <li className='py-1'>Attention to detail and organizational skills to manage multiple tasks and prioritize responsibilities effectively.</li>
+        <li className='py-1'>Ability to work well in a team environment, collaborate with colleagues, and contribute to cross-functional projects.</li>
+        <li className='py-1'>Relevant certifications such as CompTIA Security+, Certified Information Systems Security Professional (CISSP), or Certified Ethical Hacker (CEH) are advantageous.</li>
+      </ul>
       <ParticlesContainer />
-      <div className="relative mt-8 container bg-black mx-auto group rounded-2xl overflow-hidden border-4 border-gray-500">
-        {/* <img className='-z-40 h-full w-full object-cover absolute top-0 left-0 right-0' src="photos/intro_bg.jpg" alt="Introduction Background" /> */}
-        <div className="bg-opacity-70 bg-gray-800 p-8">
-          <h1 className="text-3xl font-bold text-yellow-600 mb-8">Junior Security Analyst</h1>
-
-            <AccordionBasicExample index={3}/>
-        </div>
-      </div>
-      <Footer />  </>
+    </div>
+  </div>
+    <Footer/>
+  </>
     );
 };
 
--- frontend/FSD_project/src/components/learning/webfundamental.jsx
+++ frontend/FSD_project/src/components/learning/webfundamental.jsx
@@ -9,52 +9,17 @@ const WebFundamentals = () => {
   return (
     <div >
       <Header />
-      <div className="bg-black mt-20 container ">
+      <ParticlesContainer />
+      <div className="relative mt-8 container bg-black mx-auto group rounded-2xl overflow-hidden border-4 border-gray-500">
+        {/* <img className='-z-40 h-full w-full object-cover absolute top-0 left-0 right-0' src="photos/intro_bg.jpg" alt="Introduction Background" /> */}
         <div className="bg-opacity-70 bg-gray-800 p-8">
-          <h1 className="text-3xl font-bold text-yellow-500 mb-8">Web Fundamentals</h1>
-          <section className="mb-8">
-<h2 className="text-2xl font-bold text-gray-400 mb-4">About</h2>
-<p className="text-gray-200">
-  Welcome to the Web Application Security Path! This learning path is designed to teach you the essentials of attacking web applications. The path is divided into four sections, each focusing on different aspects of web security. Feel free to explore and check off the topics as you progress through your learning journey.
-</p>
-</section>
+          <h1 className="text-3xl font-bold text-yellow-600 mb-8">Web Fundamentals</h1>
 
-<section className="mb-8">
-<h2 className="text-2xl font-bold text-gray-400 mb-4">Section 1: How The Web Works</h2>
-<p className="text-gray-200">
-This section provides fundamental knowledge about the functioning of the web. It covers topics such as Domain Name System (DNS) in detail, an in-depth understanding of Hypertext Transfer Protocol (HTTP), insights into how websites work, and a comprehensive overview of putting these concepts together.          </p>
-
-</section>
-
-<section className="mb-8">
-<h2 className="text-2xl font-bold text-gray-400 mb-4">Section 2: Introduction to Web Hacking</h2>
-<p className="text-gray-200">
-  {`In this section, you will delve into the essentials of web hacking. Topics include navigating through an application, discovering content, enumerating subdomains, bypassing authentication, understanding Insecure Direct Object Reference (IDOR), dealing with File Inclusion, an introduction to Server-Side Request Forgery (SSRF), and exploiting common vulnerabilities like Cross-site Scripting (XSS) and SQL Injection.`}
-</p>
-<form>
-  <ul className="list-disc ml-8 text4gray-700">
-  </ul>
-</form>
-</section>
-
-<section className="mb-8">
-<h2 className="text-2xl font-bold text-gray-400 mb-4">Section 3: Security Tools - Burp Suite</h2>
-<p className="text-gray-200">
-  {`This section focuses on one of the industry-standard tools, Burp Suite. You'll start with the basics and progress to using advanced features such as Repeater, Intruder, and other modules. Additionally, you'll explore extensions that enhance the functionality of Burp Suite.`}
-</p>
-</section>
-
-<section className="mb-8">
-<h2 className="text-2xl font-bold text-gray-400 mb-4">Section 4: Web Hacking Fundamentals</h2>
-<p className="text-gray-200">
-  {`Building upon the basics, this section reinforces your understanding of how websites work and HTTP. It revisits Burp Suite basics and explores the latest version of the OWASP Top 10 for 2021. Practical exercises with OWASP Juice Shop, understanding upload vulnerabilities, and navigating through challenges like "Pickle Rick" will solidify your web hacking fundamentals.`}
-</p>
-</section>
+            <AccordionBasicExample index={3}/>
         </div>
       </div>
-      <ParticlesContainer/>
       <Footer />
-    </div>
+      </div>
   );
 };
 
